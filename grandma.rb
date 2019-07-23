0d68adc0e63e60afdda37e0c145d52bf78c263fe
Madelyn Deshield <madelynwallop@gmail.com>
2019-07-23 21:49:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(morning)
   if morning== morning.upcase
   return "NO, NOT SINCE 1938!"
-elseif morning =="I LOVE YOU GRANDMA!"
-  return " I LOVE YOU TOO PUMPKIN"
+elsif morning =="I LOVE YOU GRANDMA!"
+  return "I LOVE YOU TOO PUMPKIN!"
 else
   return "HUH?! SPEAK UP, SONNY!"
 end
